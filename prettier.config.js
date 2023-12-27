e8d09eded9af5cee9bfcbf994ae2c0474e0f29aa
adyo <yingdong.wang@definesys.com>
2023-12-27 22:59:34 +0800

    init

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	tabWidth: 2,
+	useTabs: true,
+	semi: true,
+	singleQuote: true,
+	bracketSpacing: true,
+	jsxBracketSameLine: false,
+	arrowParens: 'always',
+	proseWrap: 'never',
+	htmlWhitespaceSensitivity: 'strict',
+	endOfLine: 'lf',
+	eslintIntegration: true,
+	trailingComma: 'none'
+};
